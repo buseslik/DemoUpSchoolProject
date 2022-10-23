5335a886fa604f82c5702ca325de20590d1ef8cc
eslik <eslik@DESKTOP-K6VMUD6>
2022-10-23 16:57:31 +0300

    Add project files.

---
+++ DemoUpSchoolProject/Controllers/ServicesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpSchoolProject.Models.Entities;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class ServicesController : Controller
+    {
+        UpSchoolDbPortfolioEntities db = new UpSchoolDbPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblServices.ToList();
+            return View(values);
+        }
+
+        public Ac
+    }
+}
